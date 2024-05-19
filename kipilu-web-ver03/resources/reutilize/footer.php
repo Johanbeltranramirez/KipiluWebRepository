5945ce0e360970b1f1ab97be8d89f17b861604d9
Johanbeltranramirez <johanframirez13@gmail.com>
2024-05-19 15:16:29 -0500

    dev-front: add changes, refactor menu and commentarios

---
+++ kipilu-web-ver03/resources/reutilize/footer.php
@@ -0,0 +1,36 @@
+<!-- footer.php -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+<link rel="stylesheet" href="../css/men_fot.css">
+
+<footer id="contacto">
+    <div class="contenedor footer-content">
+        <div class="contact-us">
+            <h2 class="bland">Acerca de nosotros</h2><br>
+            <p style="text-align: justify;">Un hogar de refugio de perros y gatos es un lugar donde se brinda cuidado,
+                atención y amor a animales que han sido abandonados, maltratados o perdidos. Estos hogares se dedican a
+                proporcionar un ambiente seguro y cómodo para los animales mientras esperan ser adoptados por una familia
+                amorosa.</p>
+            <p><strong>Contacto:</strong></p>
+            <p>Teléfono: 3143638383</p>
+            <p>Correo: hogarusmerescate@gmail.com</p>
+        </div>
+
+        <div class="social-media">
+            <h2 class="bland">Redes<br>sociales</h2>
+
+            <a href="https://www.facebook.com/profile.php?id=100084248649661&mibextid=ZbWKwL" target="_blank"
+               class="social-media-icon">
+                <i class="fab fa-facebook"></i>
+            </a>
+            <a href="https://twitter.com/ManuelM16762431" target="_blank" class="social-media-icon">
+                <i class="fab fa-x"></i>
+            </a>
+            <a href="https://www.instagram.com/juancm484?r=nametag" target="_blank" class="social-media-icon">
+                <i class="fab fa-instagram"></i>
+            </a>
+        </div>
+    </div>
+    <!--no eliminar linea-->
+    <div class="line"></div>
+    <!--no eliminar linea-->
+</footer>
---
+++ kipilu-web-ver03/resources/views/comentarios_historias.php
@@ -0,0 +1,152 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KIPILÚ - Comentarios</title>
+    <!--BOOTSTRAP-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css"
+          rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9"
+          crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/comentarios_historias.css">
+    <!--ICONOS-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <!--JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
+            crossorigin="anonymous"></script>
+    <script defer src="../js/Comentarios.js"></script>
+</head>
+<body>
+<!--Nav(navegacion)-->
+<?php include '../reutilize/menu.php'; ?>
+<!--Cierre Nav(navegacion)-->
+
+<main>
+<article class="caja">
+    <section class="cajaderecha">
+        <div class="container mt-5">
+            <div class="row justify-content-center">
+                <div class="col-md-10">
+                    <div class="form-container">
+                        <h1 class="text-center">COMENTARIOS</h1>
+                        <h5 class="text-center">Hola!</h5>
+                        <h5 class="text-center">Déjanos tu opinión</h5>
+                        <form method="POST" action="">
+                            <div class="form-group">
+                                <label for="Nombre">Nombre:</label>
+                                <input type="text" class="form-control" name="Nombre" placeholder="Digite un nombre" maxlength="30" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="Apellido">Apellido:</label>
+                                <input type="text" class="form-control" name="Apellido" placeholder="Digite un apellido" maxlength="30" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="Comentario">Comentario:</label>
+                                <textarea class="form-control" name="Comentario" placeholder="Digite su comentario" maxlength="150" required></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-primary btn-block">Enviar comentario</button>
+                        </form>
+                    </div>
+
+                    <h2 class="mt-5 text-center">Poste de comentarios</h2>
+                    <hr style="border: 1px solid #A52020;">
+                    <ul class="list-group Comentaristas-list show-Comentaristas">
+                        <?php
+                        $query = "SELECT * FROM " . $Comentaristas->table_name;
+                        $stmt = $db->prepare($query);
+                        $stmt->execute();
+
+                        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                            echo "<li class='list-group-item'>";
+                            echo "<div class='comment-container'>";
+                            echo "<strong>" . $row['Nombre'] . " " . $row['Apellido'] . "</strong><br>";
+                            echo "<div class='comment-text'>" . $row['Comentario'] . "</div>";
+                            echo "</div>";
+                            echo "</li>";
+                        }
+                        ?>
+                    </ul>
+                    <button class="btn btn-primary btn-block show-comments">Mostrar Comentarios</button>
+                    <button class="btn btn-primary btn-block hide-comments">Ocultar Comentarios</button>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="cajaizquierda">
+        <article class="poste">
+            <p>Historias de éxito</p>
+            <div class="clip"></div>
+            <div class="carta">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="images/HDE_MATEO.jpg" class="img-fluid custom-image rounded-start" alt="Imagen1">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <p class="card-text"><small class="text-muted">Fecha • 03/04/2023</small></p>
+                            <h5 class="card-title">Mateo</h5>
+                            <p class="card-text">Desde que lo adoptamos nuestra hija se ha vuelto más alegre y conversadora.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="carta">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="images/HDE_SAMUELNATA.jpg" class="img-fluid custom-image rounded-start" alt="Imagen2">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <p class="card-text"><small class="text-muted">Fecha • 01/04/2022</small></p>
+                            <h5 class="card-title">Samuel y Nata</h5>
+                            <p class="card-text">Hemos tenido la maravillosa oportunidad de explorar nuevos lugares gracias a Max, tiene un espíritu aventurero.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="carta">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="images/HDE_ALEJA.webp" class="img-fluid custom-image rounded-start" alt="Imagen3">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <p class="card-text"><small class="text-muted">Fecha • 03/04/2023</small></p>
+                            <h5 class="card-title">Aleja</h5>
+                            <p class="card-text">Ahora que tengo a mi compañero canino no me he sentido sola, he comenzado a tener una vida más entretenida.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="carta">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="../assets/img/HDE_MONI.jpg" class="img-fluid custom-image rounded-start" alt="Imagen4">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <p class="card-text"><small class="text-muted">Fecha • 25/08/2022</small></p>
+                            <h5 class="card-title">Moni</h5>
+                            <p class="card-text">Cada que llego a casa sé que mi amigo peludo me estará esperando con una gran sonrisa.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </article>
+    </section>
+</article>
+</main>
+
+<!--Footer(pie de pag)-->
+<?php include '../reutilize/footer.php'; ?>
+<!--Footer(pie de pag)-->
+
+</body>
+</html>
