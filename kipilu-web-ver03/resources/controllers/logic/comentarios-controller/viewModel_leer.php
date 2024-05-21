5c0091bcbbedd5d1e4c6fd3eafc734f1f8122714
judynataliacl <juncorrea@soy.sena.edu.co>
2024-05-21 00:15:50 -0500

    estiilo

--- kipilu-web-ver03/resources/controllers/logic/comentarios-controller/viewModel_leer.php
+++ kipilu-web-ver03/resources/controllers/logic/comentarios-controller/viewModel_leer.php
@@ -15,7 +15,7 @@ class CommentsViewModel {
 
     public function __construct() {
 
-        $this->api = new ApiKipilu('http://192.168.1.9:3000/api/');
+        $this->api = new ApiKipilu('http://192.168.128.3:3000/api/');
 
     }
 
@@ -23,7 +23,7 @@ class CommentsViewModel {
         try {
             // Hacer una solicitud GET a la API para obtener los comentarios
 
-            $response = file_get_contents('http://192.168.1.9:3000/api/comentaristas');
+            $response = file_get_contents('http://192.168.128.3:3000/api/comentaristas');
 
             
             // Decodificar la respuesta JSON
