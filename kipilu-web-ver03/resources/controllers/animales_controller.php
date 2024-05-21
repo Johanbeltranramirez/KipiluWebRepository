67474cee7ab8de6c1dac2e1398c75d536716a83e
Johanbeltranramirez <114593412+Johanbeltranramirez@users.noreply.github.com>
2024-05-20 19:02:58 -0500

    Update animales_controller.php

--- kipilu-web-ver03/resources/controllers/animales_controller.php
+++ kipilu-web-ver03/resources/controllers/animales_controller.php
@@ -3,16 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
     <title>KIPILU - CRUD Animales</title>
-=======
-    <title>KIPILU - CRUD ANIMALES Leer Animales</title>
-    <script type="text/javascript">
-        function confirmar() {
-            return confirm('¿Estás seguro? Se eliminarán los datos.');
-        }
-    </script>
->>>>>>> c971c2572b5f56a1be9e518fd548bfb42afd5250
     <!--PROPIO-->
     <link rel="stylesheet" href="../css/controllers_styles/animal_controller.css">
     <link rel="icon" href="../../assets/icon/logo.ico">
