3b06681e9fc5ebc83a1b9f3eb3a04a981d0198d9
Johanbeltranramirez <114593412+Johanbeltranramirez@users.noreply.github.com>
2024-05-27 21:11:09 -0500

    Update comentarios_historias.php

--- kipilu-web-ver03/resources/views/comentarios_historias.php
+++ kipilu-web-ver03/resources/views/comentarios_historias.php
@@ -8,10 +8,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-    <link rel="icon" href="../../assets/icon/logo.ico">
     <!-- Custom CSS -->
     <link rel="stylesheet" href="../css/comentarios_historias.css">
     <link rel="stylesheet" href="../css/public_comments.css">
+    <link rel="icon" href="../../assets/icon/logo.png">
     <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <!-- jQuery -->
