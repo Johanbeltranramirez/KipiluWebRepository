9be9679e78feb8cc95e257f601ab9130510c70e6
Johanbeltranramirez <114593412+Johanbeltranramirez@users.noreply.github.com>
2024-05-27 20:48:01 -0500

    Update crear_animal.php

--- kipilu-web-ver03/resources/controllers/crear_animal.php
+++ kipilu-web-ver03/resources/controllers/crear_animal.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../../assets/icon/admin_icon.png">
     <title>KIPILU - CRUD ANIMALES Crear Animal</title>
     <link rel="stylesheet" href="../css/controllers_styles/formulario_crear.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
