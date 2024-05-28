a6f6dd7ab98ff90d74a67935628ed7145ee060b9
Johanbeltranramirez <114593412+Johanbeltranramirez@users.noreply.github.com>
2024-05-27 21:09:50 -0500

    Update catalogo_caninos.php

--- kipilu-web-ver03/resources/views/catalogo_caninos.php
+++ kipilu-web-ver03/resources/views/catalogo_caninos.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>KIPILU - Catálogo de Caninos</title>
-    <link rel="icon" href="img/logo.ico">
     <!--BOOSTRAP-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
@@ -12,6 +11,7 @@
     <link rel="stylesheet" href="css/men_fot.css">
     <!--PROPIO-->
     <link rel="stylesheet" href="../css/catalogo_caninos.css">
+    <link rel="icon" href="../../assets/icon/logo.png">
     <!--LLAMAR ICONOS-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
