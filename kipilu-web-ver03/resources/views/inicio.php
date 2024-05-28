886ea0b71a18304ae9c8ab3a286178b057696341
Johanbeltranramirez <114593412+Johanbeltranramirez@users.noreply.github.com>
2024-05-27 21:38:36 -0500

    Update inicio.php

--- kipilu-web-ver03/resources/views/inicio.php
+++ kipilu-web-ver03/resources/views/inicio.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>KIPILU - Inicio</title>
-    <link rel="icon" href="img/logo.ico">
     <!--BOOSTRAP-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
@@ -13,6 +12,7 @@
     <link rel="stylesheet" href="css/men_fot.css">
     <!--LLAMAR ICONOS-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="icon" href="../../assets/icon/logo.png">
    <!-- Enlazar el archivo CSS externo -->
    <link rel="stylesheet" href="../css/inicio.css">
    
