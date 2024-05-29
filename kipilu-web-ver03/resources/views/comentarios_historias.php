7b06a75f0e2bddc193ad70b7493542850f2b143a
Johanbeltranramirez <114593412+Johanbeltranramirez@users.noreply.github.com>
2024-05-29 12:16:47 -0500

    Update comentarios_historias.php

--- kipilu-web-ver03/resources/views/comentarios_historias.php
+++ kipilu-web-ver03/resources/views/comentarios_historias.php
@@ -52,7 +52,7 @@ function validateText(input) {
                                 </div>
                                 <div class="form-group">
                                     <label for="Comentario">Comentario:</label>
-                                    <textarea class="form-control" id="Comentario" name="Comentario" placeholder="Digite su comentario" maxlength="150" required></textarea>
+                                    <textarea class="form-control" id="Comentario" name="Comentario" placeholder="Digite su comentario" maxlength="70" required></textarea>
                                 </div>
                                 <button type="submit" class="btn btn-primary btn-block">Enviar comentario</button>
                             </form>
