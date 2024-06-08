3cb984e372d8ab1266196e2ea38734265ff84d98
Johanbeltranramirez <114593412+Johanbeltranramirez@users.noreply.github.com>
2024-06-07 23:59:19 -0500

    Update app.php

--- kipilu-web/app.php
+++ kipilu-web/app.php
@@ -2,13 +2,6 @@
 
 $page = isset($_GET['page']) ? $_GET['page'] : 'inicio';
 
-
-// Incluir la página solicitada
-$file = 'resources/views/inicio' . $page . '.php';
-if (file_exists($file)) {
-    include $file;
-} else {
-    include '404.php';
-}
+include 'resources/views/inicio.php';
 
 ?>
