66c6a4f25975106c330815ab192871b1319aaaa4
nicolsita12 <114593458+nicolsita12@users.noreply.github.com>
2024-06-22 15:49:03 -0500

    Update viewModel_eliminar.php

--- kipilu-web/resources/controllers/logic/animales-controller/viewModel_eliminar.php
+++ kipilu-web/resources/controllers/logic/animales-controller/viewModel_eliminar.php
@@ -6,7 +6,6 @@ class AnimalsViewModel {
     public function __construct() {
         // Definimos la URL base de la API
         $this->apiBaseUrl = 'https://kipilubackendrepository-3.onrender.com/api/';
-
     }
 
     public function deleteAnimal($id) {
@@ -19,21 +18,22 @@ class AnimalsViewModel {
                 ]
             ];
             $context = stream_context_create($options);
-            $response = @file_get_contents($url, false, $context); 
+            $response = @file_get_contents($url, false, $context);
 
             if ($response === false) {
-                throw new Exception('No se puede eliminar el animal porque está asociado a uno o más formularios.');
+                throw new Exception('No se puede eliminar el animal.');
             }
 
             $responseData = json_decode($response, true);
-            if (isset($responseData['success']) && $responseData['success']) {
-                // Redirigir a la página principal o mostrar un mensaje de éxito
-                echo "<script language='JavaScript'>alert('Animal eliminado exitosamente.'); location.assign('../../animales_controller.php');</script>";
-                return true;
-            } else {
-                echo "<script language='JavaScript'>alert('Error al eliminar el animal: " . $responseData['message'] . "');</script>";
-                return false;
+
+            if (isset($responseData['message'])) {
+                throw new Exception('Error al eliminar el animal: ' . $responseData['message']);
             }
+
+            // Redirigir a la página principal o mostrar un mensaje de éxito
+            echo "<script language='JavaScript'>alert('Animal eliminado exitosamente.'); location.assign('../../animales_controller.php');</script>";
+            return true;
+            
         } catch (Exception $e) {
             echo "<script language='JavaScript'>alert('" . $e->getMessage() . "'); location.assign('../../animales_controller.php');</script>";
             return false;
